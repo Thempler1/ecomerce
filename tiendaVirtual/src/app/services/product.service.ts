1bb5b0a3853bc6a5e6818a592566acae93c4cb84
thempler1 <tevi.leyton@gmail.com>
2021-06-24 17:50:06 -0400

    👽️ update service

--- tiendaVirtual/src/app/services/product.service.ts
+++ tiendaVirtual/src/app/services/product.service.ts
@@ -6,11 +6,11 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   public productList = [
-    { name: "C³ EQUALZ X TKC KIWI SWITCHES", description: "Los Switches Kiwi son miembros de la familia de switches frutales de TKC, junto a los Tangerines.", type: "Táctiles", pack: 10, price: 13490 },
-    { name: "KAIHUA HALO TRUE MECHANICAL SWITCHES", description: "Combinan los mejores atributos de los switches silenciosos y de click,lo que da como resultado una consistencia y suavidad casi perfecta con cada pulsación de tecla.", type: "Táctiles", pack: 64, price: 57290 },
-    { name: "ALPACA LINEARS SWITCHES", description: "Switches lineales suaves con resortes de 62g chapados en oro (abajo hacia afuera). Los colores se basan en la combinación de colores SA Bliss que nos trajo Minterly.", type: "Lineales", pack: 10, price: 14190 },
-    { name: "KAILH NK CREAMS SWITCHES", description: "lineales de los mas aclamados en el hobby. Con una case y un stem hechos de POM autolubricante, este switch ofrece una experiencia suave y única.", type: "Lineales", pack: 10, price: 10490 },
-    { name: "Gateron Yellow Switches", description: "Switches mecánicos lineales amarillos, la versión más reciente de un clásico Gateron, perfecto para jugar. Compatible con SMD y LED de paso.", type: "Lineales", pack: 10, price: 6490 }
+    { name: "C³ EQUALZ X TKC KIWI SWITCHES", description: "Los Switches Kiwi son miembros de la familia de switches frutales de TKC, junto a los Tangerines.", type: "Táctiles", pack: 10, price: 13490, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/427/349/products/c3_equalz_x_tkc_kiwi_switches__1616035474_370ddada_progressive1-092da1760eeea5dd4816225172296946-320-0.jpg" },
+    { name: "KAIHUA HALO TRUE MECHANICAL SWITCHES", description: "Combinan los mejores atributos de los switches silenciosos y de click,lo que da como resultado una consistencia y suavidad casi perfecta con cada pulsación de tecla.", type: "Táctiles", pack: 64, price: 57290 , image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/427/349/products/011-c35f6554e4a57e720016107504361329-240-0.jpg"},
+    { name: "ALPACA LINEARS SWITCHES", description: "Switches lineales suaves con resortes de 62g chapados en oro (abajo hacia afuera). Los colores se basan en la combinación de colores SA Bliss que nos trajo Minterly.", type: "Lineales", pack: 10, price: 14190, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/427/349/products/webp-net-resizeimage1-f98e5dbbadc688fd3516123805846713-240-0.jpg"},
+    { name: "KAILH NK CREAMS SWITCHES", description: "lineales de los mas aclamados en el hobby. Con una case y un stem hechos de POM autolubricante, este switch ofrece una experiencia suave y única.", type: "Lineales", pack: 10, price: 10490, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/427/349/products/creams1-080f14e088ba87d4b616123820565903-240-0.jpg" },
+    { name: "Gateron Yellow Switches", description: "Switches mecánicos lineales amarillos, la versión más reciente de un clásico Gateron, perfecto para jugar. Compatible con SMD y LED de paso.", type: "Lineales", pack: 10, price: 6490, image:"https://d2r9epyceweg5n.cloudfront.net/stores/001/427/349/products/yellow1-476ce5be32d5140e9b16129198028776-240-0.jpg" }
   ];
 
   constructor() { }
