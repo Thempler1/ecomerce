525b16c832786b94f23398529d52d1d2cacbec5c
thempler1 <tevi.leyton@gmail.com>
2021-06-24 17:04:25 -0400

    ⚡️ add routing

--- tiendaVirtual/src/app/app-routing.module.ts
+++ tiendaVirtual/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { OrdenCompraComponent } from './components/orden-compra/orden-compra.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'products', component: OrdenCompraComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tiendaVirtual/src/app/app.module.ts
+++ tiendaVirtual/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdenCompraComponent } from './components/orden-compra/orden-compra.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrdenCompraComponent
+    OrdenCompraComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
