3e1dad844a2d6735591354ad0bc2e47ea5a0e914
thempler1 <tevi.leyton@gmail.com>
2021-06-28 23:09:57 -0400

    🚚 add path to payment component

--- tiendaVirtual/src/app/app-routing.module.ts
+++ tiendaVirtual/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ShipAndPayComponent } from './components/ship-and-pay/ship-and-pay.component';
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'home', component: HomeComponent},
-  {path:'products', component: ProductsComponent}
+  {path:'products', component: ProductsComponent},
+  {path:'payment', component: ShipAndPayComponent}
 ];
 
 @NgModule({
