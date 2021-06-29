bdb5a44778d9f5555ea8444234ac588652bca501
thempler1 <tevi.leyton@gmail.com>
2021-06-28 21:11:21 -0400

    💄 add pagination to most viewed

--- tiendaVirtual/src/app/components/home/home.component.ts
+++ tiendaVirtual/src/app/components/home/home.component.ts
@@ -9,11 +9,26 @@ import { ProductService } from 'src/app/services/product.service';
 export class HomeComponent implements OnInit {
 
   public productList: any = null;
+  public firsthalf  : any = [];
+  public secondhalf : any = [];
 
   constructor( private productService : ProductService) { }
 
   ngOnInit(): void {
     this.productList= this.productService.getProducts();
+    this.spliceList();
+  }
+
+  spliceList(){
+    let counter = 0;
+    this.productList.forEach((element: any) => {
+      if (counter < 3) {
+        this.firsthalf.push(element);
+      } else {
+        this.secondhalf.push(element);
+      }
+      counter++
+    });
   }
 
 }
